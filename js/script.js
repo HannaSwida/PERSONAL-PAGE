1c585c51d352c274b2dca74ab53652568d8fc65c
Hanna Świda <hanna-swida@hotmail.com>
2019-01-31 05:58:25 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+//PRELOADER
+$(window).on('load', function() {
+    $('#status').delay(200).fadeOut();
+    $('#preloader').delay(350).fadeOut();
+})
+
+//PROGRESS BAR
+$(function() {
+    $("#progress-elements").waypoint(function(){
+        $(".progress-bar").each(function(){
+            $(this).animate({
+                width:$(this).attr("aria-valuenow") + '%'
+            },1000);
+        });
+        this.destroy();
+    },{
+        offset:'bottom-in-view'
+    });
+
+});
+const el = document.querySelector( '.counter' )
+
+// Start counting, do this on DOM ready or with Waypoints.
+
+$(function() {
+    $("#stats").waypoint(function(){
+        $('#counter-1').animateNumber({ number: 23 },3500);
+        $('#counter-2').animateNumber({ number: 3 },3500);
+        $('#counter-3').animateNumber({ number: 10},3500);
+        $('#counter-4').animateNumber({ number: 9001},3500);
+        this.destroy();
+    },{
+        offset:'bottom-in-view'
+    });
+
+});
