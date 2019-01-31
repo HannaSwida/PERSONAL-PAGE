25a067d0ad555e3e616a8e99a00abac1d2174b90
Hanna Świda <hanna-swida@hotmail.com>
2019-01-31 06:42:04 +0100

    CSS : Add animations etc

--- js/script.js
+++ js/script.js
@@ -3,7 +3,13 @@
 $(window).on('load', function() {
     $('#status').delay(200).fadeOut();
     $('#preloader').delay(350).fadeOut();
-})
+});
+$('a[href*="#about"]').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
+});
 
 //PROGRESS BAR
 $(function() {
@@ -35,3 +41,4 @@ $(function() {
     });
 
 });
+
